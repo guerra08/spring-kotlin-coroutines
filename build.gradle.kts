7ae229d97c4407856359cc27f878a0d68d5400bb
Bruno Guerra <gguerrabruno@outlook.com>
2022-01-30 19:27:52 -0300

    Adds Postgres with Docker

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,8 +27,9 @@ dependencies {
     implementation ("org.springframework.boot:spring-boot-starter-validation")
 
     developmentOnly("org.springframework.boot:spring-boot-devtools")
-    runtimeOnly("com.h2database:h2")
-    runtimeOnly("io.r2dbc:r2dbc-h2")
+
+    runtimeOnly("io.r2dbc:r2dbc-postgresql")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.projectreactor:reactor-test")
 
