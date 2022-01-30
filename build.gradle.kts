fbf1eead565bec77ff8dbdca2d5866d99a40e6a6
Bruno Guerra <gguerrabruno@outlook.com>
2022-01-30 10:17:06 -0300

    Add validation

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,9 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+
+    implementation ("org.springframework.boot:spring-boot-starter-validation")
+
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     runtimeOnly("com.h2database:h2")
     runtimeOnly("io.r2dbc:r2dbc-h2")
--- src/main/kotlin/com/example/springkotlincoroutines/domain/Product.kt
+++ src/main/kotlin/com/example/springkotlincoroutines/domain/Product.kt
@@ -3,24 +3,32 @@ package com.example.springkotlincoroutines.domain
 import com.fasterxml.jackson.databind.annotation.JsonSerialize
 import org.springframework.data.annotation.Id
 import org.springframework.data.relational.core.mapping.Table
+import javax.validation.constraints.NotEmpty
 
 @Table("products")
 @JsonSerialize
 data class Product(
     @Id
     val id: Long?,
+    @field:NotEmpty
     val name: String,
-    val brand: String
+    @field:NotEmpty
+    val brand: String,
+    val price: Double
 )
 
 data class CreateProductDTO(
+    @field:NotEmpty
     val name: String,
-    val brand: String
+    @field:NotEmpty
+    val brand: String,
+    val price: Double
 )
 
 data class PatchProductDTO(
     val name: String?,
-    val brand: String?
+    val brand: String?,
+    val price: Double?
 )
 
 /**
@@ -29,11 +37,13 @@ data class PatchProductDTO(
 fun CreateProductDTO.toProductEntity(id: Long? = null) = Product(
     id = id,
     name = this.name,
-    brand = this.brand
+    brand = this.brand,
+    price = this.price
 )
 
 fun PatchProductDTO.toProductEntity(original: Product) = Product(
     id = original.id,
     name = this.name ?: original.name,
-    brand = this.brand ?: original.brand
+    brand = this.brand ?: original.brand,
+    price = this.price ?: original.price
 )
